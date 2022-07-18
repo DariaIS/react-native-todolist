8ad7cec4411579dbf8e6c0b48bb2f878db0e07ad
DariaIS <dariastarodubova58@yandex.ru>
2022-07-18 19:16:09 +0300

    did items list

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View style={styles.tasksWrapper}>
+                <Text style={styles.sectionTitle}>Today's tasks</Text>
+                <View style={styles.items}>
+                    <Task text={'Task 1'} />
+                    <Task text={'Task 2'} />
+                    <Task text={'Task 3'} />
+                </View>
+            </View>
+
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#E8EAED',
+    },
+    tasksWrapper: {
+        paddingTop: 40,
+        paddingHorizontal: 20,
+    },
+    sectionTitle: {
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    items: {
+        marginTop: 30,
+    }
 });
